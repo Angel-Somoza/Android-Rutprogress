3a9c74adae9a7b9420b807466f765f6ab1f85b91
Angel Somoza <somozacontrerasangeldavid@gmail.com>
2025-03-23 15:33:46 -0600

    codigo base Rutprogress

---
+++ app/src/main/java/com/example/rutprogress/view/Login_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.rutprogress.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.rutprogress.R
+
+class Login_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
